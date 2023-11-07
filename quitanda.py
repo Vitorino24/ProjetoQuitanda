df100db9a948072277ddff8edd8278a9207ca928
Vitorino24 <131809295+Vitorino24@users.noreply.github.com>
2023-11-07 14:59:18 -0300

    arrumar

--- quitanda.py
+++ quitanda.py
@@ -4,42 +4,41 @@ import uuid
 
 app = Flask(__name__)
 app.secret_key = "quitandazezinho"
-
 usuario = "usuario"
 senha = "senha"
 login = False
 
-# FUNÇÃO PARA VERIFICAR SESSÃO
+#FUNÇÃO PARA VERIFICAR SESSÃO
 def verifica_sessao():
+
     if "login" in session and session["login"]:
         return True
     else:
         return False
-
-# CONEXÃO COM O BANCO DE DADOS
+    
+#CONEXÃO COM O BANCO DE DADOS
 def conecta_database():
     conexao = sql.connect("db_quitanda.db")
     conexao.row_factory = sql.Row
     return conexao
 
-# INICIAR O BANCO DE DADOS
+#INICIAR O BANCO DE DADOS
 def iniciar_db():
     conexao = conecta_database()
     with app.open_resource('esquema.sql', mode='r') as comandos:
         conexao.cursor().executescript(comandos.read())
-        conexao.commit()
-        conexao.close()
+    conexao.commit()
+    conexao.close()
 
 # ROTA DA PÁGINA INICIAL
 @app.route("/")
 def index():
-    iniciar_db()  # Corrigido: chamar a função iniciar_db
+    iniciar_db()
     conexao = conecta_database()
     produtos = conexao.execute('SELECT * FROM produtos ORDER BY id_prod DESC').fetchall()
     conexao.close()
     title = "Home"
     return render_template("home.html", produtos=produtos, title=title)
 
-# FINAL DO CÓDIGO - EXECUTANDO O SERVIDOR
-if __name__ == "__main__":
-    app.run(debug=True)
+# FINAL DO CODIGO - EXECUTANDO O SERVIDOR
+app.run(debug=True)
