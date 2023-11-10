b6e0215d17c3e41d72d820dc74397c12541d98b4
Vitorino24 <131809295+Vitorino24@users.noreply.github.com>
2023-11-10 14:06:35 -0300

    atualizacao parte 245

--- quitanda.py
+++ quitanda.py
@@ -38,6 +38,50 @@ def logout():
     session.clear()
     return redirect('/')
 
+#Rota de exclusão
+@app.route("/excluir/<id>")
+def excluir(id):
+    if verifica_sessao():
+        id = int(id)
+        conexao = conecta_database()
+        conexao.execute('DELETE FROM produtos WHERE id_prod = ?',(id,))
+        conexao.commit()
+        conexao.close()
+        return redirect('/adm')
+    else:
+        return redirect("/login")
+    
+#Criar a rota do editar
+@app.route("/editprodutos/<id_prod>")
+def editar(id_prod):
+    if verifica_sessao():
+        iniciar_db()
+        conexao = conecta_database()
+        produtos = conexao.execute('SELECT * FROM produtos WHERE id_prod = ?',(id_prod,)).fetchall()
+        conexao.close()
+        title = "Edição de produtos"
+        return render_template("editprdutos.html",produtos=produtos, title=title)
+    else:
+        return redirect("/login")
+    
+#Criar a rota para tratar a edição
+@app.route("/editarprodutos", methods=['POST'])
+def editprod():
+    id_prod = request.form['id_prod']
+    nome_prod=request.form['nome_prod']
+    desc_prod=request.form['desc_prod']
+    preco_prod=request.form['preco_prod']
+    img_prod=request.form['img_prod']
+    id_foto=str(uuid.uuid4().hex)
+    filename=id_foto+nome_prod+'.png'
+    img_prod.save("static/img/prdutos/"+filename)
+    conexao = conecta_database()
+    conexao.execute('UPDATE produtos SET nome_prod = ?, desc_prod = ?, preco_prod = ?, img_prod = ? WHERE id_prod = ?',(nome_prod,desc_prod,preco_prod,filename,id_prod))
+    conexao.commit()
+    conexao.close()
+    return redirect('/adm')
+
+
 #Rota da pág de cadastro no banco
 @app.route("/cadastro",methods=["post"])
 def cadastro():
@@ -73,7 +117,8 @@ def adm():
         conexao = conecta_database()
         produtos = conexao.execute('SELECT * FROM produtos ORDER BY id_prod DESC').fetchall()
         conexao.close()
-        title = render_template("adm.html", produtos=produtos, title=title)
+        title = "Administração"
+        return render_template("adm.html", produtos=produtos, title=title)
     else:
         return redirect("/login")
 
